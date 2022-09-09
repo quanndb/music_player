39efd2c93e9b0ab7f6fd063d26cafe88b5f14f78
quanndb <110159302+quanndb@users.noreply.github.com>
2022-09-09 18:47:02 +0700

    Update main.js

--- main.js
+++ main.js
@@ -64,6 +64,12 @@ const app ={
             image: 'https://lyricvn.com/wp-content/uploads/2021/11/65dc1a9b418217d2b23ca86c7792f541.jpg',
             path: './mp3/ct.mp3'
         }
+        {
+            name: 'Bình nguyên vô tận',
+            singer: 'Pho lo ti no',
+            image: 'https://i.pinimg.com/originals/d7/7e/ad/d77eadafecac7302bea7cd5a9a95bc2b.jpg',
+            path: './mp3/binhnguyen.mp3'
+        }
     ],
     setConfig: function(key,value){
         this.config[key]=value
